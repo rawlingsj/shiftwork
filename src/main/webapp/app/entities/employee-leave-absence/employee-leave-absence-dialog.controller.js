2cc07d4a495a85390a0ef3a7ec928ccd4ba90a8c
Seven Vu <vunguyenthe1976@gmail.com>
2016-10-20 18:33:24 +0700

    code change for "Modify Employee Leave Absence UI"

--- src/main/webapp/app/entities/employee-leave-absence/employee-leave-absence-dialog.controller.js
+++ src/main/webapp/app/entities/employee-leave-absence/employee-leave-absence-dialog.controller.js
@@ -5,14 +5,14 @@
         .module('shiftworkApp')
         .controller('EmployeeLeaveAbsenceDialogController', EmployeeLeaveAbsenceDialogController);
 
-    EmployeeLeaveAbsenceDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', 'entity', 'EmployeeLeaveAbsence', 'EmployeeAbsentReason', 'Employee'];
+    EmployeeLeaveAbsenceDialogController.$inject = ['$timeout', '$scope', '$filter', '$stateParams', '$uibModalInstance', 'entity', 'EmployeeLeaveAbsence', 'EmployeeAbsentReason', 'Employee'];
 
-    function EmployeeLeaveAbsenceDialogController ($timeout, $scope, $stateParams, $uibModalInstance, entity, EmployeeLeaveAbsence, EmployeeAbsentReason, Employee) {
+    function EmployeeLeaveAbsenceDialogController ($timeout, $scope, $filter, $stateParams, $uibModalInstance, entity, EmployeeLeaveAbsence, EmployeeAbsentReason, Employee) {
         var vm = this;
         vm.employeeLeaveAbsence = entity;
         vm.employeeabsentreasons = EmployeeAbsentReason.query();
         vm.employees = Employee.query();
-
+        vm.isSaving = false;
         $timeout(function (){
             angular.element('.form-group:eq(1)>input').focus();
         });
@@ -47,5 +47,57 @@
         vm.openCalendar = function(date) {
             vm.datePickerOpenStatus[date] = true;
         };
+
+        if( vm.employeeLeaveAbsence.absentFrom == null) {
+            vm.employeeLeaveAbsence.absentFrom = new Date();
+        }
+
+        if( vm.employeeLeaveAbsence.absentTo == null) {
+            vm.employeeLeaveAbsence.absentTo = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+            vm.employeeLeaveAbsence.absentTo.setHours(0,0,0,0);
+        }        
+        var selectedEmployee = false;
+        var selectedReason = false;
+        vm.showEmployee = function() {
+               var name = '';
+               if(vm.employees.length == 0 || vm.employeeLeaveAbsence.employee == null)  {
+                name =  'Employee';
+                selectedEmployee = false;
+               }
+               else {
+                   var selected = $filter('filter')(vm.employees, {id: vm.employeeLeaveAbsence.employee.id});
+                   if(vm.employeeLeaveAbsence.employee.id && selected.length){
+                        name = selected[0].name;
+                        selectedEmployee = true;
+                   }
+               }
+               if(selectedEmployee && selectedReason) {
+                     vm.isSaving = true;
+               }
+               return name;
+        };
+
+        vm.showReason = function() {
+                var name = 'Not set';
+                selectedReason = true;
+                if(vm.employeeabsentreasons.length == 0)  {
+                    selectedReason = false;
+                }
+                else if(vm.employeeLeaveAbsence.reason == null){
+                    name = vm.employeeabsentreasons[0].name;//select default value
+                    vm.employeeLeaveAbsence.reason = vm.employeeabsentreasons[0];
+                }
+                else{
+                    var selected = $filter('filter')(vm.employeeabsentreasons, {id: vm.employeeLeaveAbsence.reason.id});
+                    if(vm.employeeLeaveAbsence.reason.id && selected.length){
+                       name = selected[0].name;
+                    }
+               }
+               if(selectedEmployee && selectedReason) {
+                    vm.isSaving = true;
+               } 
+               return name;
+        };
+
     }
 })();
